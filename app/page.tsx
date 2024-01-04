1df0be96db68be9c9cdc6770e4dd89fb6797728b
Nathaniel Budikey <nthnlbdky916@gmail.com>
2024-01-05 07:56:57 +0800

    Update page
    refractor code

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-export default function Page() {
+function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -51,3 +51,5 @@ export default function Page() {
     </main>
   );
 }
+
+export default Page;
