0b2456e5118a496fa71906005a4f20ff4e5f8ecf
Nathaniel Budikey <nthnlbdky916@gmail.com>
2024-01-04 18:43:48 +0800

    Updated page.tsx
    Added paragraph

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,7 @@ function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
+        <p>This is it</p>
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
